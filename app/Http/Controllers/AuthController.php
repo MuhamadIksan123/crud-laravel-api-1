c390d9c3684790c844b8c79d90a419d74575c9ef
Muhamad Iksan <muhamadiksan351@gmail.com>
2025-01-23 19:10:24 +0700

    feat: authorization and role_id

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
+
+class AuthController extends Controller
+{
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+            'device_name' => 'required'
+        ]);
+
+        $user = User::where('email', $request->email)->first();
+        if (!$user || !Hash::check($request->password, $user->password)) {
+            throw ValidationException::withMessages([
+                'email' => ['The provided credentials are incorrect']
+            ]);
+        }
+
+        $user->tokens()->delete();
+
+        return $user->createToken($request->device_name)->plainTextToken;
+    }
+
+    function logout(Request $request)
+    {
+        $request->user()->currentAccessToken()->delete();
+
+        return response()->json(['message' => 'logout success']);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\TodoController;
 use Illuminate\Http\Request;
@@ -11,12 +12,17 @@ Route::get('/user', function (Request $request) {
 
 Route::get('/todos', [TodoController::class, 'index']);
 Route::get('/todos/{id}', [TodoController::class, 'show']);
-Route::post('/todos', [TodoController::class, 'store']);
-Route::patch('/todos/{id}', [TodoController::class, 'update']);
-Route::delete('/todos/{id}', [TodoController::class, 'destroy']);
+Route::post('/todos', [TodoController::class, 'store'])->middleware(['auth:sanctum', 'can:post-todo']); // Vendor
+Route::patch('/todos/{id}', [TodoController::class, 'update'])->middleware(['auth:sanctum', 'can:edit-todo']); // Editor
+Route::delete('/todos/{id}', [TodoController::class, 'destroy'])->middleware(['auth:sanctum', 'can:delete-todo']); //admin
 
 Route::get('/categories', [CategoryController::class, 'index']);
 Route::get('/categories/{id}', [CategoryController::class, 'show']);
 Route::post('/categories', [CategoryController::class, 'store']);
 Route::patch('/categories/{id}', [CategoryController::class, 'update']);
 Route::delete('/categories/{id}', [CategoryController::class, 'destroy']);
+
+Route::prefix('auth')->group(function() {
+    Route::post('/login', [AuthController::class, "login"]);
+    Route::get('/logout', [AuthController::class, "logout"])->middleware('auth:sanctum');
+});
